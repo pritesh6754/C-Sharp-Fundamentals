80fc6b0e6d4cc5d75bcc76ffd8fcf50c056d1404
pritesh6754 <86052547+pritesh6754@users.noreply.github.com>
2024-10-13 12:49:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace Hello
+{
+    class Program
+    {
+        //static void Greet(string name)
+        //{
+        //    Console.WriteLine("Good Morning " + name);
+        //}
+        static void Main(string[] args)
+        {
+            //int pritesh = 10;
+            //float jadhav = 10.5F;
+            //bool isGreat = true;
+            //char c = 'c';
+            //string s = "This is string";
+            //Console.WriteLine("Hello, World" +  pritesh);
+            //Console.WriteLine(pritesh);
+            //Console.WriteLine(jadhav);
+            //Console.WriteLine(isGreat);
+            //Console.WriteLine(c);
+            //Console.WriteLine(s);
+
+            //Console.WriteLine("Enter your name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hey hello " + name);
+            //Console.WriteLine("How many candies do you want?");
+            //string candy = Console.ReadLine();
+            //Console.WriteLine("You will get 4 more candies" + (Convert.ToInt32(candy) + 4));
+
+            //int a = 5; int b = 6;
+            //Console.WriteLine("The value of a + b is : " + (a + b));
+            //Console.WriteLine("The value of a - b is : " + (a - b));
+            //Console.WriteLine("The value of a * b is : " + (a * b));
+            //Console.WriteLine("The value of a / b is : " + (a / b));
+
+            //string hello = "My name is Pritesh";
+            //Console.WriteLine(hello.Length);
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello.ToLower());
+
+            //string name = Console.ReadLine();
+            //string candies = Console.ReadLine();
+            //Console.WriteLine($"Your name is {name}. You will get {candies} candies.");
+
+            //int age = 24;
+            //if (age > 18)
+            //{
+            //    Console.WriteLine("You  can  drive..");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You  cannot  drive !!");
+            //}
+
+            //int age = 20;
+            //switch (age)
+            //{
+            //    case 18:
+            //        Console.WriteLine("Please wait for an year..");
+            //        break;
+
+            //    case 20:
+            //        Console.WriteLine("You are 20's");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Enjoy!");
+            //        break;
+
+            //}
+
+            //int num = 0;
+            //while (num < 10)
+            //{
+            //    Console.WriteLine(num + 1);
+            //    num++;
+            //}
+
+            //Greet("harry");
+            //Greet("larry");
+
+
+        }
+
+    }
+}
